b76339c243f5c2adb9d9c9e4c67aab86de819e0c
Sean Marquez <capsulecorplab@gmail.com>
2022-01-16 03:26:00 -0700

    rename `bundle` to `bundler` in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 # Install RubyGems & run Jekyll build
-docker run --rm -v $PWD:/srv -w /srv ruby:3.1.0-buster bundle i && bundle exec jekyll build
+docker run --rm -v $PWD:/srv -w /srv ruby:3.1.0-buster bundler i && bundler exec jekyll build
