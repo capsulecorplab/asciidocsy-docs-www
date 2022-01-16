cb288952fd06cd18fc1ff4cf4b75c08fa261ba9b
Sean Marquez <capsulecorplab@gmail.com>
2022-01-15 20:33:04 -0700

    add working directory to docker command

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 # Run Jekyll server
-docker run --rm -v "$PWD:/srv/jekyll" ruby:3.0 bundle exec jekyll build
+docker run --rm -v $PWD:/srv -w /srv ruby:3.0 bundle i && bundle exec jekyll build
