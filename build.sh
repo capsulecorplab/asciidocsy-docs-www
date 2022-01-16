e5131d61e4054201d6f2ebad6c69152abcac6ec4
Sean Marquez <capsulecorplab@gmail.com>
2022-01-15 20:50:14 -0700

    separate commands for gem install & Jekyll build

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/sh
 
-# Run Jekyll server
-docker run --rm -v $PWD:/srv -w /srv ruby:3.0 bundle i && bundle exec jekyll build
+# Install RubyGems
+docker run --rm -v $PWD:/srv -w /srv ruby:3.0 bundle i
+
+# Run Jekyll build
+docker run --rm -v $PWD:/srv -w /srv ruby:3.0 bundle exec jekyll build
