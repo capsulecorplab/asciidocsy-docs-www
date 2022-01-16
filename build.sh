e035837c4207be81770ab244499837ff6d46bd9d
Sean Marquez <capsulecorplab@gmail.com>
2022-01-16 03:54:55 -0700

    execute bundle commands as string arg in bash

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 # Install RubyGems & run Jekyll build
-docker run --rm -v $PWD:/srv -w /srv ruby:3.1.0-buster which bundler && which gem
+docker run --rm -v $PWD:/srv -w /srv ruby:3.1.0-buster /bin/bash -c "bundle i && bundle exec jekyll build"
