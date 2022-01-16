fe323026dbb9e3bbb0d4edbf524bb67ca6190420
Sean Marquez <capsulecorplab@gmail.com>
2022-01-16 03:36:02 -0700

    echo $PATH && ls /usr/local/bin

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 # Install RubyGems & run Jekyll build
-docker run --rm -v $PWD:/srv -w /srv ruby:3.1.0-buster /usr/local/bin/bundler i && /usr/local/bin/bundler exec jekyll build
+docker run --rm -v $PWD:/srv -w /srv ruby:3.1.0-buster echo $PATH && ls /usr/local/bin
