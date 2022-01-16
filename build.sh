da305c6108846de59fd0cb247e583179738a42fe
Sean Marquez <capsulecorplab@gmail.com>
2022-01-16 03:44:36 -0700

    locate bundler & gem binaries

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 # Install RubyGems & run Jekyll build
-docker run --rm -v $PWD:/srv -w /srv ruby:3.1.0-buster echo $PATH && ls /usr/local/bin
+docker run --rm -v $PWD:/srv -w /srv ruby:3.1.0-buster which bundler && which gem
